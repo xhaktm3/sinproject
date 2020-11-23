ee5d0626906b49e38b66ddcee2bd0db9236c1f64
xhaktm3 <xhaktm3@kaist.ac.kr>
2020-11-23 20:36:40 +0900

    new front

---
+++ server2/cra/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          BAP-YAK
+        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/main">
+          <button className="App-login"> 
+            LOGIN
+          </button>
+        </Link>
+      </header>
+    </div>
+
+    
+  );
+}
+
+export default App;
+
+
+
+
