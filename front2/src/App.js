56a4a3ffd05b7fd631114ea318364df8654743ac
xhaktm3 <xhaktm3@kaist.ac.kr>
2020-11-23 22:00:55 +0900

    a

--- front2/src/App.js
+++ front2/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,20 +7,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          BAP-YAK
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/main">
+          <button className="App-login"> 
+            LOGIN
+          </button>
+        </Link>
       </header>
     </div>
+
+    
   );
 }
 
--- front2/src/index.js
+++ front2/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Main from './components/Main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <div id='header'>
+      <Link to="/">HOME</Link>
+    </div>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/main">
+        <Main />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
